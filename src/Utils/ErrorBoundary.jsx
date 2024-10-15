5c4c280e55254d989ee948d5834c819fb26b942f
marketLube <marketlubes@gmail.com>
2024-10-16 01:06:17 +0530

    sl

--- src/Utils/ErrorBoundary.jsx
+++ src/Utils/ErrorBoundary.jsx
@@ -11,9 +11,7 @@ class ErrorBoundary extends Component {
     return { hasError: true };
   }
 
-  componentDidCatch(error, info) {
-    console.error("ErrorBoundary caught an error:", error, info);
-  }
+  componentDidCatch(error, info) {}
 
   render() {
     if (this.state.hasError) {
